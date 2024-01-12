f103a6e5591cb58d0510262ab39f47206f21abc6
ukik <muhamadduki@gmail.com>
2024-01-12 07:29:43 +0700

    1/12/2024

---
+++ config/badaso-watch-tables.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'badaso_users',
+    'branches',
+    'campus_bookings',
+    'campus_courses',
+    'campus_lecturers',
+    'campus_rooms',
+    'cars',
+    'cinema_genres',
+    'cinema_movies',
+    'cinema_payments',
+    'cinema_seats',
+    'cinema_shows',
+    'cinema_studios',
+    'cinema_tickets',
+    'employees',
+    'payments',
+    'rentals',
+];
---
+++ database/seeders/Badaso/CRUD/BadasoDeploymentOrchestratorSeeder.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Database\Seeders\Badaso\CRUD;
+
+use Illuminate\Database\Seeder;
+use Uasoft\Badaso\Traits\Seedable;
+
+class BadasoDeploymentOrchestratorSeeder extends Seeder
+{
+    use Seedable;
+
+    protected $seedersPath = 'database/seeders/Badaso/CRUD/';
+
+    /**
+     * Run the database seeders.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->seed(BranchesCRUDDataTypeAdded::class);
+        $this->seed(BranchesCRUDDataRowAdded::class);
+        $this->seed(CarsCRUDDataTypeAdded::class);
+        $this->seed(CarsCRUDDataRowAdded::class);
+        $this->seed(EmployeesCRUDDataTypeAdded::class);
+        $this->seed(EmployeesCRUDDataRowAdded::class);
+        $this->seed(RentalsCRUDDataTypeAdded::class);
+        $this->seed(RentalsCRUDDataRowAdded::class);
+        $this->seed(PaymentsCRUDDataTypeAdded::class);
+        $this->seed(PaymentsCRUDDataRowAdded::class);
+        
+        
+        $this->seed(CinemaStudiosCRUDDataTypeAdded::class);
+        $this->seed(CinemaStudiosCRUDDataRowAdded::class);
+        $this->seed(CinemaShowsCRUDDataTypeAdded::class);
+        $this->seed(CinemaShowsCRUDDataRowAdded::class);
+        $this->seed(CinemaSeatsCRUDDataTypeAdded::class);
+        $this->seed(CinemaSeatsCRUDDataRowAdded::class);
+        $this->seed(CinemaTicketsCRUDDataTypeAdded::class);
+        $this->seed(CinemaTicketsCRUDDataRowAdded::class);
+        $this->seed(CinemaPaymentsCRUDDataTypeAdded::class);
+        $this->seed(CinemaPaymentsCRUDDataRowAdded::class);
+        $this->seed(CinemaGenresCRUDDataTypeAdded::class);
+        $this->seed(CinemaGenresCRUDDataRowAdded::class);
+        $this->seed(CinemaMoviesCRUDDataDeleted::class);
+        $this->seed(CinemaMoviesCRUDDataTypeAdded::class);
+        $this->seed(CinemaMoviesCRUDDataRowAdded::class);
+        
+        
+        $this->seed(KampusRoomsCRUDDataDeleted::class);
+        $this->seed(CampusRoomsCRUDDataTypeAdded::class);
+        $this->seed(CampusRoomsCRUDDataRowAdded::class);
+        $this->seed(CampusCoursesCRUDDataTypeAdded::class);
+        $this->seed(CampusCoursesCRUDDataRowAdded::class);
+        
+        
+        $this->seed(CampusLecturesCRUDDataDeleted::class);
+        $this->seed(CampusLecturersCRUDDataTypeAdded::class);
+        $this->seed(CampusLecturersCRUDDataRowAdded::class);
+        
+        
+        $this->seed(CampusBookingsCRUDDataDeleted::class);
+        $this->seed(CampusBookingsCRUDDataTypeAdded::class);
+        $this->seed(CampusBookingsCRUDDataRowAdded::class);
+    }
+}
